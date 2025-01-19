e21eb4d9381f4799d7b5e7d5edd33e8ee842834e
Khang <khangnh1904@gmail.com>
2025-01-19 14:22:16 +0700

    Done Project

---
+++ Test_Tourkit_FontEnd/Pages/Product.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace Test_Tourkit_FontEnd.Pages
+{
+    public class ProductModel : PageModel
+    {
+        
+    }
+
+}
---
+++ Test_Tourkit_FontEnd/Pages/ProductCategory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test_Tourkit_FontEnd.Pages
+{
+    public class ProductCategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
